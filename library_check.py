1bf4da8f279a7b75c0e9baa4470bacfd5443c50d
Cagri Esen <esencgr@gmail.com>
2020-10-12 11:19:16 +0300

    update

--- library_check.py
+++ library_check.py
@@ -2,15 +2,20 @@
 import cv2
 import numpy
 import matplotlib
-import pandas
 import scipy
-#import beautifulsoup4
+
+import pandas
+import selenium
+from selenium import webdriver
+
+print( pandas.__version__)
+print( selenium.__version__)
+print( webdriver.__version__)
 
 print( cv2.__version__)
 print( numpy.__version__)
 print( matplotlib.__version__)
 print( pandas.__version__)
 print( scipy.__version__)
-# print( beautifulsoup4.__version__)
-
+print( selenium.__version__)
 
