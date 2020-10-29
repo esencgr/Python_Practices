c98bcb62b0264e80d09238112c2550757fd4c902
Cagri Esen <esencgr@gmail.com>
2020-10-29 12:27:50 +0300

    add

---
+++ x.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+
+us = "cgr"
+ps = "1111"
+chance = 5 
+
+while( True ):
+    
+    user = input( "enter username : " ) 
+    pwd = input( "enter password : " )
+    
+    if( user != us and pwd == ps ):
+        print( " username is uncorrect ! ".center( 50,'*') )
+        chance -= 1 
+
+    elif( user == us and pwd != ps ):
+        print( " password is uncorrect ! ".center( 50,'*') )
+        chance -= 1 
+
+    elif( user != us and pwd != ps ):
+        print( " username and password is incorrect ! ".center( 50,'*') )
+        chance -= 1 
+
+    else:
+        print( " succesfull login ! ".center( 50,'-') )
+        break
+    
+    if( chance == 0 ):
+        print( " dont have any chance ! ".center( 50,'x') )
+        break
+
