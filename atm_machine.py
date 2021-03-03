a0fcb4e9150fc08d721c619415f77ab6cf270c1f
Cagri Esen <esencgr@gmail.com>
2021-03-03 22:24:47 +0300

    add

--- atm_machine.py
+++ atm_machine.py
@@ -1,34 +1,34 @@
 # -*- coding: utf-8 -*-
-# işlem menusu 
-#    1 - bakiye sorgulama 
-#    2 - para yatırma 
-#    3 - para çekme 
+# işlem menusu
+#    1 - bakiye sorgulama
+#    2 - para yatırma
+#    3 - para çekme
 #    q - çıkış
 
 money = 1000
 
 while( True ):
-    choise = input( "choise : " )   
-    
+    choise = input( "choise : " )
+
     if( choise == "q" ):
-        break 
-    
+        break
+
     if( choise == "1" ):
         print( f"total balance = {money}" )
-    
+
     elif( choise == "2" ):
         ex = int(input( "how much money invest want you : " ))
         money += ex 
         print( f"update balance = {money}" )
-        
+
     elif( choise == "3" ):
         ex = int(input( "how much withdraw money want you : " ))
 
         if( money - ex <= 0 ):
             print( f"balance is not enough and now balance = {money} " )
-            continue 
-        money -= ex 
+            continue
+        money -= ex
         print( f"update balance = {money}" )
-        
+
     else:
         print( "invalid process " )
