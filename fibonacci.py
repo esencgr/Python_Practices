a3e0c7d5a4678fe6615e525f644a30258e273958
Cagri Esen <esencgr@gmail.com>
2021-03-21 00:05:11 +0300

    add

--- fibonacci.py
+++ fibonacci.py
@@ -19,3 +19,20 @@ while a < 100:
     
 print( ls )
 ls.clear()
+
+# cube = lambda x: x**3
+
+# def fibonacci(n):
+#     if n == 0:
+#         return []
+#     if n == 1:
+#         return [0]
+#     prev = 0
+#     cur = 1
+#     out = [prev, cur]
+    
+#     for _ in range(n-2):
+#         prev, cur = cur, prev + cur
+#         out.append(cur)
+        
+#     return out
